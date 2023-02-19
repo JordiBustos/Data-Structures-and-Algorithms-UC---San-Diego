b6da6e9140106d497a0da2a0b76d50c981b27217
JordiBustos <jordi.bustos193771@alumnos.info.unlp.edu.ar>
2023-02-18 21:24:05 -0300

    added pdfs, removed java c++ and solved tree-height

--- 2 - Data Structures/week1_basic_data_structures/1_brackets_in_code/check_brackets.py
+++ 2 - Data Structures/week1_basic_data_structures/1_brackets_in_code/check_brackets.py
@@ -11,20 +11,27 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
+
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
-
-        if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+            opening_brackets_stack.append(next)
+        else:
+            if next in ")]}":
+                # Process closing bracket, write your code here
+                top = opening_brackets_stack.pop()
+                if not are_matching(top, next):
+                    return i+1
+            
+    return 'Success' if len(opening_brackets_stack) == 0 else (i+1)
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
+    print(mismatch) 
+        
 
 
 if __name__ == "__main__":
--- 2 - Data Structures/week1_basic_data_structures/2_tree_height/tree-height.py
+++ 2 - Data Structures/week1_basic_data_structures/2_tree_height/tree-height.py
@@ -3,23 +3,50 @@
 import sys, threading
 sys.setrecursionlimit(10**7) # max depth of recursion
 threading.stack_size(2**27)  # new thread will get stack of such size
+from collections import deque
+
+class Node:
+    def __init__(self, name):
+        self.name = name
+        self.children = []
+
+    def add_child(self, child_index):
+        self.children.append(child_index)
+               
 
 class TreeHeight:
         def read(self):
                 self.n = int(sys.stdin.readline())
                 self.parent = list(map(int, sys.stdin.readline().split()))
 
+        def __createTree(self):
+                nodes = [Node(i) for i in range(self.n)] 
+                
+                for i in range(self.n):
+                        if self.parent[i] == -1:
+                                self.root = i
+                        else:
+                                nodes[self.parent[i]].add_child(nodes[i])
+                        
+                return nodes
+
         def compute_height(self):
                 # Replace this code with a faster implementation
-                maxHeight = 0
-                for vertex in range(self.n):
-                        height = 0
-                        i = vertex
-                        while i != -1:
-                                height += 1
-                                i = self.parent[i]
-                        maxHeight = max(maxHeight, height);
-                return maxHeight;
+                tree = self.__createTree()
+                d = deque()
+
+                d.append(tree[self.root])
+                height = 0
+
+                while len(d):
+                        height += 1
+                        for i in range(len(d)):
+                                node = d.popleft()
+
+                                for child in node.children:
+                                        d.append(child)
+
+                return height 
 
 def main():
   tree = TreeHeight()
